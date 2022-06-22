b5b932a1fa9b638107c817b890faf8567c86d4c2
akan mayowa <50503664+akanmayowa@users.noreply.github.com>
2022-06-22 10:52:35 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
+  
+Route::middleware('auth:api')->group( function () {
 
 Route::resource('users', 'App\Http\Controllers\Api\UserController');
 Route::resource('users/user-wallet-transaction', 'App\Http\Controllers\Api\UserController@userWalletTransactionDetails');
-
 
 Route::resource('wallets', 'App\Http\Controllers\Api\WalletController');
 Route::resource('wallets/user-wallet-details', 'App\Http\Controllers\Api\WalletController@usersWalletDetails');
@@ -22,6 +23,9 @@ Route::resource('wallets/user-wallet-details', 'App\Http\Controllers\Api\WalletC
 Route::resource('transactions', 'App\Http\Controllers\Api\TransactionController');
 Route::resource('transactions/wallet-counter', 'App\Http\Controllers\Api\TransactionController@walletCounter');
 Route::resource('transactions/transfer-fund', 'App\Http\Controllers\Api\TransactionController@TransferFunds');
+
+});
+
 
 
 
